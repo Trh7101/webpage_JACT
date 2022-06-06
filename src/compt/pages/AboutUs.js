cb12dd0f8186c3320e09bc5edeea1976727be889
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-06-06 13:57:51 -0500

    06/06 1:57PM

--- src/compt/pages/AboutUs.js
+++ src/compt/pages/AboutUs.js
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import Cara1 from "../img/Programador1.jpeg"
+import Cara2 from '../img/Programador2.jpeg'
+import Cara3 from "../img/Programador3.jpeg"
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -17,10 +20,78 @@ const theme = createTheme();
 
 export default function AboutUs() {
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth='xl'>
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-      </Container>
-    </ThemeProvider>
+    <Container maxWidth={false} disableGutters={true}>
+      <Box sx={{ bgcolor: '#cfe8fc', height: '88.9616vh' }}>
+        <Box
+          sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: { xs: 'center', md: 'flex-start' },
+          m: 0,
+          minWidth: { md: 350 },
+          backgroundColor: 'primary.dark',
+          height: 300,
+          }}>
+            <Box
+              component="img"
+              sx={{
+              m: 2,
+              height: 270,
+              width: 350,
+              maxHeight: { xs: 350, md: 233 },
+              maxWidth: { xs: 233, md: 185 },
+              }}
+              alt="Programador más chico."
+              src={Cara1}/>
+              Soy Chava el chavo que no puede con las chavas, pero se programar
+        </Box>
+        <Box
+          sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: { xs: 'center', md: 'flex-start' },
+          m: 0,
+          minWidth: { md: 350 },
+          backgroundColor: 'primary.dark',
+          height: 300,
+          }}>
+            <Box
+              component="img"
+              sx={{
+              m: 2,
+              height: 270,
+              width: 350,
+              maxHeight: { xs: 350, md: 233 },
+              maxWidth: { xs: 233, md: 185 },
+              }}
+              alt="Programador más mamado."
+              src={Cara2}/>
+            Hola me gusta la comida y programar, tambien entreno un chingo. PD. Me di de baja de mi materia
+        </Box>
+        <Box
+          sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: { xs: 'center', md: 'flex-start' },
+          m: 0,
+          minWidth: { md: 350 },
+          backgroundColor: 'primary.dark',
+          height: 300,
+          }}>
+            <Box
+              component="img"
+              sx={{
+              m: 2,
+              height: 270,
+              width: 350,
+              maxHeight: { xs: 350, md: 233 },
+              maxWidth: { xs: 233, md: 185 },
+              }}
+              alt="Programador más otako."
+              src={Cara3}/>
+              Hola yo no se programar pero esta chido, me gusta la miku
+        </Box>
+      </Box>
+    </Container>
   );
 }

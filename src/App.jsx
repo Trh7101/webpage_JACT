67e1a3d67b1c59e292823e538c9cddac30be3314
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-06-06 11:48:10 -0500

    06/06

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navigation from './compt/Navigation';
-import Home from './compt/pages/home/Home';
-import SignUp from './compt/pages/signUp/Signup';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './compt/pages/Home';
+import SignUp from './compt/pages/Signup';
+import AboutUs from './compt/pages/AboutUs';
 
-function App() {
+
+export default function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/signup' element={<SignUp />}/>        
+        <Route path='/signup' element={<SignUp />}/>       
+        <Route path='/UsAbout' element={<AboutUs/>}/> 
       </Routes>
       
     </Router>
   );
 }
 
-export default App;
--- src/compt/pages/Signup.js
+++ src/compt/pages/Signup.js
@@ -1,3 +1,4 @@
+//This is a template extracted from: https://mui.com/material-ui/getting-started/templates/ credits for the autor. 
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -12,19 +13,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const theme = createTheme();
 
@@ -58,25 +46,15 @@ export default function SignUp() {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12}>
                 <TextField
                   autoComplete="given-name"
                   name="firstName"
                   required
                   fullWidth
                   id="firstName"
-                  label="First Name"
+                  label="Nombre de Usuario"
                   autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
                 />
               </Grid>
               <Grid item xs={12}>
@@ -84,7 +62,7 @@ export default function SignUp() {
                   required
                   fullWidth
                   id="email"
-                  label="Email Address"
+                  label="Dirección de mail"
                   name="email"
                   autoComplete="email"
                 />
@@ -94,16 +72,10 @@ export default function SignUp() {
                   required
                   fullWidth
                   name="password"
-                  label="Password"
-                  type="password"
+                  label="Contraseña"
+                  type="password" 
                   id="password"
                   autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
             </Grid>
@@ -113,18 +85,18 @@ export default function SignUp() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Registrate
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                  Ya tienes una cuenta? inicia sesión
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
+        
       </Container>
     </ThemeProvider>
   );

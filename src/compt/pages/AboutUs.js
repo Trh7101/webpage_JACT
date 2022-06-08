719b550a5587a620a80d32fd1cf2ad75f65197f5
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-06-08 12:13:48 -0500

    08/06

--- src/compt/pages/AboutUs.js
+++ src/compt/pages/AboutUs.js
@@ -26,58 +26,77 @@ const theme = createTheme();
 export default function AboutUs() {
   return (
     <Container maxWidth={false} disableGutters={true}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',  bgcolor: 'rgb(155,173,183)' }}>
-        <Box
-            component="img"
-            sx={{ m: 2, maxHeight: { xs: 275, md: 250 }, maxWidth: { xs: 300, md: 370 } }}
-            alt="Player Select."
-            src={TextoHead}/>
-        <Box
-            component="img"
-            sx={{ m: 2, maxHeight: { xs: 0, md: 1000 }, maxWidth: { xs: 0, md: 1000 } }}
-            alt="Player Select."
-            src={play1}/>
-        <Box sx={{
-          display: 'flex', flexDirection: 'row', alignItems: 'center',
-          m: 1, maxWidth: 800, backgroundColor: 'rgb(118,66,138)',
-        }}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',  bgcolor: 'rgb(155,173,183)' }}>
+        <Grid  sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',  bgcolor: 'transparent'}}>
           <Box
-            component="img"
-            sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
-            alt="Programador más chico."
-            src={Cara1}/>
-          <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
-            Student at Tecnológico de Monterrey. I consider myself very curious. I like to push my creative skills creating Pixel-art and music. Passionate about technological advancements, music and entertainment, I'm interested on AI, smart processes and optimization.
-          </Typography>
-        </Box>
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 0, md: 1000 }, maxWidth: { xs: 0, md: 1000 } }}
+              alt="Player Select."
+              src={play1}/>
+          <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 0, md: 1000 }, maxWidth: { xs: 0, md: 1000 } }}
+              alt="Player Select."
+              src={play2}/>
+          <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 0, md: 1000 }, maxWidth: { xs: 0, md: 1000 } }}
+              alt="Player Select."
+              src={play3}/>
+          <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 0, md: 1000 }, maxWidth: { xs: 0, md: 1000 } }}
+              alt="Player Select."
+              src={play4}/>
+        </Grid>
+        <Grid  sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',  bgcolor: 'transparent'}}>
+          <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 275, md: 250 }, maxWidth: { xs: 300, md: 370 } }}
+              alt="Player Select."
+              src={TextoHead}/>
+          <Box sx={{
+            display: 'flex', flexDirection: 'row', alignItems: 'center',
+            m: 1, maxWidth: 800, backgroundColor: 'rgb(118,66,138)',
+          }}>
+            <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
+              alt="Programador más chico."
+              src={Cara1}/>
+            <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
+              Student at Tecnológico de Monterrey. I consider myself very curious. I like to push my creative skills creating Pixel-art and music. Passionate about technological advancements, music and entertainment, I'm interested on AI, smart processes and optimization.
+            </Typography>
+          </Box>
 
-        <Box sx={{
-          display: 'flex', flexDirection: 'row', alignItems: 'center',
-          m: 1, maxWidth: 800, backgroundColor: 'rgb(55,148,110)',
-        }}>
-          <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
-            Me di de baja. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a sem odio. Vestibulum scelerisque arcu consectetur nisi molestie, eget ullamcorper erat ullamcorper. Donec cursus, nunc vel venenatis viverra, enim neque sagittis metus, in facilisis elit nulla sed lorem. Ut egestas, dolor ac congue faucibus, mi turpis pulvinar ligula, eu tincidunt magna massa in enim.
-          </Typography>
-          <Box
-            component="img"
-            sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
-            alt="Programador más mamado."
-            src={Cara2}/>
-            
-        </Box>
-        <Box sx={{
-          display: 'flex', flexDirection: 'row', alignItems: 'center',
-          m: 1, maxWidth: 800, backgroundColor: 'rgb(172,50,50)',
-        }}>
-          <Box
-            component="img"
-            sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
-            alt="Programador más otako."
-            src={Cara3}/>
-          <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
-            Student at Tecnológico de Monterrey. I consider myself very curious. I use to get inspired by playing games and listening to music. Passionate about technological advancements, music and entertainment, I'm interested on AI and Security.      Mr. Milanesa (2022). Linkedin. https://www.linkedin.com/in/salvador-federico-milanés-braniff-160631238/
-          </Typography>
-        </Box>
+          <Box sx={{
+            display: 'flex', flexDirection: 'row', alignItems: 'center',
+            m: 1, maxWidth: 800, backgroundColor: 'rgb(55,148,110)',
+          }}>
+            <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
+              Me di de baja. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a sem odio. Vestibulum scelerisque arcu consectetur nisi molestie, eget ullamcorper erat ullamcorper. Donec cursus, nunc vel venenatis viverra, enim neque sagittis metus, in facilisis elit nulla sed lorem. Ut egestas, dolor ac congue faucibus, mi turpis pulvinar ligula, eu tincidunt magna massa in enim.
+            </Typography>
+            <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
+              alt="Programador más mamado."
+              src={Cara2}/>
+              
+          </Box>
+          <Box sx={{
+            display: 'flex', flexDirection: 'row', alignItems: 'center',
+            m: 1, maxWidth: 800, backgroundColor: 'rgb(172,50,50)',
+          }}>
+            <Box
+              component="img"
+              sx={{ m: 2, maxHeight: { xs: 275, md: 233 }, maxWidth: { xs: 200, md: 185 } }}
+              alt="Programador más otako."
+              src={Cara3}/>
+            <Typography component="abouttext" variant="h6" m={2} alignSelf='flex-start'>
+              Student at Tecnológico de Monterrey. I consider myself very curious. I use to get inspired by playing games and listening to music. Passionate about technological advancements, music and entertainment, I'm interested on AI and Security.      Mr. Milanesa (2022). Linkedin. https://www.linkedin.com/in/salvador-federico-milanés-braniff-160631238/
+            </Typography>
+          </Box>
+        </Grid>
       </Box>
     </Container>
   );

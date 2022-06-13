e434444b4084ff1255360b6f300f12a4d1f04fb7
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-06-13 00:10:56 -0500

    a

--- src/compt/pages/Home.js
+++ src/compt/pages/Home.js
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import BackgroundVid from '../img/noot_noot.mp4';
+import BackgroundVid from '../img/vid1_racemaker.mp4';
 
 
 export default function Home() {
@@ -18,14 +18,27 @@ export default function Home() {
       <video className='videoTag' autoPlay loop muted>
         <source src={BackgroundVid} type='video/mp4' />
       </video>
-      <Typography variant="h6" m={2} sx={{ borderTop: 50, borderTopColor: 'transparent' }}>
-        EL JUEGO DONDE PUEDES CREAR TU PROPIA PISTA
-      </Typography>
-      <ul className='ready'>
-        <Button color="inherit" variant='contained' href="/signup">
-          Play Racing Car
-        </Button>
-      </ul>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', borderTop: 200, borderTopColor: 'transparent'}}>
+        <Typography fontFamily={'PressStart2P'} variant="h6" m={2}>
+          Crea o elige tu pista!
+        </Typography>
+        <Typography fontFamily={'PressStart2P'} variant="h6" m={2}>
+          Corre!!
+        </Typography>
+        <Typography fontFamily={'PressStart2P'} variant="h6" m={2}>
+          Mejora tu pista!
+        </Typography>
+        <Typography fontFamily={'PressStart2P'} variant="h6" m={2}>
+          Mejora tus tiempos!
+        </Typography>
+      </Box>
+      <Box sx={{borderTop: 100, borderTopColor: 'transparent'}}>
+        <ul className='ready'>
+          <Button color="inherit" variant='contained' href="/signup" size='large'>
+            Play Race Maker
+          </Button>
+        </ul>
+      </Box>
     </Container>
 
   );

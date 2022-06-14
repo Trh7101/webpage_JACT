a5ada97c1ca9a6eb7c1eab9c979a3cd057cf320b
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-06-13 23:11:47 -0500

    o

--- src/compt/pages/Home.js
+++ src/compt/pages/Home.js
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import BackgroundVid from '../img/PromotionalRaceMaker.mp4';
+import BackgroundVid from '../img/PromBackVid.mp4';
 import PlayB from './../BUTTONS/Play2.png';
 
 
